36dad68a17c9855fd8ab7ff7fd58337ab4d39680
Juan Tenezaca <tenj34@gmail.com>
2018-06-16 20:23:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,3 +47,12 @@ end
 def position_taken?(board, location)
   !(board[location].nil? || board[location] == " " || board[location] == "")
 end
+
+def valid_move?(board, index)
+  spot = index.to_i - 1
+  if !position_taken?(board,index) && index.between?(0,8)
+    true
+  else
+    false
+  end
+end
